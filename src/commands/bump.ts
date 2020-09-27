42c9b184affc986746becd30039b2fa8196f9441
Ivan Petrukhin <petrivah@yandex.ru>
2020-09-27 21:47:58 +0300

    Add parameters to <bump

--- src/commands/bump.ts
+++ src/commands/bump.ts
@@ -1,5 +1,33 @@
 import { Message } from 'discord.js';
+import { CronJob } from 'cron';
+
+import logger from '../logger';
+
+const tasks: Record<string, CronJob> = {};
 
 export default (message: Message, ...args: string[]): void => {
+  const [rawCommand] = message.content.split(' ');
+  const [task] = args;
+  const taskID = message.channel.id;
   message.delete();
+  if (task === 'start') {
+    try {
+      tasks[taskID].start();
+    } catch (e) {
+      logger.error(`Unable to start the task (channel: ${message.channel.id}, content: "${message.content}")`);
+    }
+  } else if (task === 'stop') {
+    try {
+      tasks[taskID].stop();
+    } catch (e) {
+      logger.error(`Unable to start the task (channel: ${message.channel.id}, content: "${message.content}")`);
+    }
+  } else if (task !== undefined) {
+    try {
+      tasks[taskID] = new CronJob(args.join(' '), () => message.channel.send(rawCommand));
+      tasks[taskID].start();
+    } catch (e) {
+      logger.error(`Incorrect cron pattern (channel: ${message.channel.id}, content: "${message.content}")`);
+    }
+  }
 };
--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ client.on('ready', () => {
 });
 
 client.on('message', (message) => {
-  const prefix = process.env.PREFIX ?? '';
+  const prefix = process.env.PREFIX ?? '<';
   const isCommand = (message.author.id === client.user?.id && message.content.startsWith(prefix));
   if (isCommand) {
     const argv = message.content.slice(prefix.length).split(' ');
