283a0bef0fb03971bdfc1649b2e39f4f4a22da5e
Ivan Petrukhin <petrivah@yandex.ru>
2020-09-27 17:46:51 +0300

    Add .env configuration file and logger

---
+++ src/logger.ts
@@ -0,0 +1,19 @@
+import { createLogger, format, transports } from 'winston';
+
+export default () => createLogger({
+  transports: [
+    new transports.Console({
+      format: format.combine(
+        format.colorize({
+          level: true
+        }),
+        format.timestamp({
+          format: 'YYYY/MM/DD hh:mm:ss'
+        }),
+        format.printf(({ level, message, timestamp }) => {
+          return `${timestamp} [${(level)}]: ${message}`;
+        })
+      )
+    })
+  ]
+});
