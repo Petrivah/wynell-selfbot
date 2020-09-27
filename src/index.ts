2cfe8d3c7d9c70184182ded8b59db5030be73470
Ivan Petrukhin <petrivah@yandex.ru>
2020-09-27 17:55:00 +0300

    Hello, world!

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 import { config } from 'dotenv';
+import { Client } from 'discord.js';
+
+import createLogger from './logger';
 
 config();
+
+const client = new Client();
+const logger = createLogger();
+
+client.on('ready', () => {
+  logger.info(`Logged in as ${client.user?.tag}`);
+});
+
+client.login(process.env.TOKEN);
